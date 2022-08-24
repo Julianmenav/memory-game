7195fe3ed733af0d5afa94921e7c801ce292f43b
Julianmenav <julianmenav2@gmail.com>
2022-08-24 21:30:17 +0200

    Add card flip animation

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,27 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import logo from "../icons/logo.svg";
 
+const SHOW_TIME_SC = 3 * 1000
 
+const Card = ({ sprite }) => {
+  const [flipped, setFlipped] = useState(false);
+  const [showPhase, setShowPhase] = useState(true);
 
-
-
-const Card = ({sprite}) => {
-  const [volteada, setVolteada] = useState(false);
-  
-
+  //ComponentDidMount
+  useEffect(() => {
+    //Tras 1 sec, muestra las cartas
+    setTimeout(() => {
+      setFlipped(true);
+      //3 segundos despues de mostrarse, vuelven a voltearse, y ya se puede jugar.
+      setTimeout(() => {
+        setFlipped(false)
+        setShowPhase(false)
+      }, SHOW_TIME_SC);
+    }, 1000);
+  }, []);
 
   const voltear = () => {
-    setVolteada(!volteada);
-    //TODO: Animation
+    if(showPhase) return
+    setFlipped(true);
+    //TODO: Comparar ID
   };
 
-  const img = <img src={sprite.img} alt="img_carta" />
-  // const style = volteada ? "buttonCard" : "pollarda"
-  return(
-  <button className="buttonCard" onClick={voltear}>
-    {volteada ? img : null}
-  </button>
 
-  )
+
+  return (
+    <div onClick={voltear} className="flip-card-button">
+      <div className={`flip-card-inner ${flipped ? "" : "hover-trigger"}`}>
+        <div className="flip-card-front">
+          <img src={sprite.img} alt="front" />
+        </div>
+        <div className="flip-card-back">
+          <img src={logo} alt="back" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  sprite: PropTypes.shape({
+    id: PropTypes.number,
+    img: PropTypes.string,
+  }),
 };
 
 export default Card;
