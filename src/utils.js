fb78c2af035599eba261e9d109d132307a8b7b4d
Julianmenav <julianmenav2@gmail.com>
2022-08-27 15:51:34 +0200

    Fix: fetch function no longer gets duplicates

--- src/utils.js
+++ src/utils.js
@@ -1,30 +1,36 @@
 const getCards = async (numberOfCards) => {
-  let result = [];
-  for (let i = 0; i < numberOfCards; i++) {
-    const randomNumber = Math.floor(Math.random() * 800);
+  let randomNumbers = [];
+  let fetchResult = [];
+  let numberOfPokemons = 151
+
+  //Get X random numbers which refer to X random pokemons sprites
+  while (randomNumbers.length < numberOfCards) {
+    console.log("obteniendo un numerito")
+    let randomNumber = Math.floor((Math.random() * numberOfPokemons)) + 1;
+    if (!randomNumbers.includes(randomNumber)) {
+      randomNumbers.push(randomNumber);
+    }
+  }
+  //Fetch those numbers to get pokemon sprites.
+  for (let randomNumber of randomNumbers) {
     const res = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${randomNumber}`
     );
     const responseJson = await res.json();
-    result.push(responseJson.sprites.front_default);
+    fetchResult.push(responseJson.sprites.front_default);
   }
-  return result.map((el, idx) => ({ id: idx, img: el }));
+  const cards = fetchResult.map((el, idx) => ({ id: idx, img: el }));
+  return cards;
 };
 
-//In: Array of objects. Out: 2 shuffle arrays of objects.
+
+//Input: Array of objects. Output: 2 shuffle arrays of objects.
 const shuffleCards = (cards) => {
-  const length = cards.length
-  const duplicatedArray = [...cards,...cards]
-  const randomArray = [...duplicatedArray].sort(() => Math.random() - 0.5)
+  const length = cards.length;
+  const duplicatedArray = [...cards, ...cards];
+  const randomArray = [...duplicatedArray].sort(() => Math.random() - 0.5);
 
-  return [randomArray.slice(0, length), randomArray.slice(length)]
-}
-
-
-
-
-
-
-
+  return [randomArray.slice(0, length), randomArray.slice(length)];
+};
 
 export { getCards, shuffleCards };
