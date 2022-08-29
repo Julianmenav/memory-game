1447127edb96e7180a127e60e95be793be2b2d37
Julianmenav <julianmenav2@gmail.com>
2022-08-29 17:53:50 +0200

    Deploy on github pages

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./routes/App";
 import Home from "./routes/Home";
-import reportWebVitals from "./reportWebVitals";
 import NoPage from "./routes/NoPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" >
           <Route index element={<Home />} />
@@ -18,11 +17,6 @@ root.render(
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
