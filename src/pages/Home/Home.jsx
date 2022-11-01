575deabe19499fea279d281f2682831b57a72ed7
Julianmenav <julianmenav2@gmail.com>
2022-11-01 16:48:34 +0100

    style: Change author anchor style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import LinkButton from "../../components/LinkButton";
-import title from "../../assets/title.png"
+import title from "../../assets/title.png";
 import "./Home.css";
-
 
 const Home = () => {
   return (
@@ -11,16 +10,17 @@ const Home = () => {
         <img src={title} alt="" />
       </div>
       <LinkButton path="game">JUGAR</LinkButton>
-      <div className="author">
-        Designed and Coded by <br />
-        <a
-          href="https://github.com/Julianmenav/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Julian Mena
-        </a>
-      </div>
+      <a
+        href="https://github.com/Julianmenav/"
+        target="_blank"
+        rel="noreferrer"
+        className="author"
+      >
+        <div >
+          Designed and Coded by <br />
+          <span className="authorName">Julian Mena</span>
+        </div>
+      </a>
     </div>
   );
 };
